a8d8e773092c1f3b0390ed3fdf437c06311bd934
RomanPastukh <116081155+RomanPastukh@users.noreply.github.com>
2022-11-21 15:00:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict"
+let roma = 'hello'
+console.log(roma)
+let sdfsdf = 'owu'
+console.log(sdfsdf)
+let rfgt = 'com'
+console.log(rfgt)
+let qasw = 'ua'
+console.log(qasw)
+console.log(roma +` `+ sdfsdf + ` `+rfgt+` `+ qasw)
+
+let num = `1`
+console.log(num )
+let qaswe = `10`
+console.log(qasw)
+let edfr = `-999`
+console.log(edfr)
+let rfgte = `123`
+console.log(rfgt)
+let tghy = `3.14`
+console.log(tghy)
+let yhju = `2.7`
+console.log(yhju)
+let azxs = `16`
+console.log(azxs)
+let dcvf= `true`
+console.log(dcvf)
+let numer = `false`
+console.log(numer)
+console.log(num + ` ` + qaswe + ` ` +edfr+ ` ` + rfgte+ ` ` +tghy+ ` ` +yhju+ ` ` +azxs+ ` ` +dcvf+ ` ` +numer)
+let fristName = 'Roman'
+let middleName = 'Serhiyovych'
+let lastName = 'Pastukh'
+console.log(fristName +` `+ middleName + ` `+lastName )
+let a = 100;
+console.log(typeof(a ))
+let b = `100` ;
+console.log(typeof(b +``))
+let c = true;
+console.log(typeof c );
