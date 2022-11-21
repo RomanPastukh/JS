f6ed4c1bbe15c6056458d78dcf29a1027832bb0c
RomanPastukh <rpastuh4@gmail.com>
2022-11-21 21:26:57 +0200

    js.hw123

---
+++ js.hw/lesson3/main.js
@@ -0,0 +1,70 @@
+// let x = 0;
+// if (x !==0) {
+//     console.log(`Yes`)
+// } else {
+//     console.log(`No`);
+// }
+
+// let time =20;
+// if (time>=0 && time<=15){
+//     console.log('first');
+// } else if (time>=16 && time<=30) {
+//     console.log('second');
+// } else if (time>=31 && time <=45) {
+//     console.log('third');
+// } else if(time>=46 && time <=60) {
+//     console.log('forth');
+// } else {
+//     console.log('error');
+// }
+
+// let day = 35;
+// day > 0 && day <=10 ? console.log('first') : day >10 && day<=20 ? console.log('second') :
+//     day>20 && day <=31 ? console.log('third') : console.log('error')
+
+
+// let num = +prompt();
+// switch (num) {
+//     case 1:
+//         console.log('sunday');
+//         break;
+//     case 2:
+//         console.log('monday');
+//         break;
+//     case 3:
+//         console.log('tuesday');
+//         break;
+//     case 4:
+//         console.log('wednesday');
+//         break;
+//     case 5:
+//         console.log('thursday');
+//         break;
+//     case 6:
+//         console.log('friday');
+//         break;
+//     case 7:
+//         console.log('satturday');
+//         break;
+//     default:
+//         console.log('error');
+
+
+// }
+
+// let a =5;
+// let b=10;
+// if (a>b) {
+//     console.log(a);
+// } else if (b>a) {
+//     console.log(b);
+// } else if (a === b) {
+//     console.log('equal');
+// } else {
+//     console.log('error');
+// }
+
+let x = 'str';
+if(!!x ==false) {
+
+}
